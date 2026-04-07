c264f860c9614c19a1150cabf6f24600ff6ca505
Pedro Guimaraes <peuvictor22@gmail.com>
2026-04-07 17:58:44 -0300

    fix: promocao manual de usuario para prestador

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   # Não precisa estar logado para ver a home
-  def index
-    @services = Service.all.order(nome: :asc)
+ def index
+  # TRUQUE TEMPORÁRIO: Promove o seu usuário assim que você abrir a Home
+  if current_user && current_user.email == "seu-email@exemplo.com"
+    current_user.update(role: "prestador")
   end
+
+  @services = Service.all
+end
 end
