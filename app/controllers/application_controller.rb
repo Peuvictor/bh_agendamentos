880ab8c8ad101168c50c88ae7b1e9bf0ee6bb4a3
Pedro Guimaraes <peuvictor22@gmail.com>
2026-04-07 10:53:43 -0300

    feat: adiciona foto, bio e endereco no cadastro inicial e atualiza strong parameters do Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
     # Libera os campos na hora de atualizar a conta (account_update)
     devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :telefone, :bio, :endereco, :avatar])
 
-    # Bônus: Se quiser que eles já preencham o nome na hora de criar a conta (sign_up)
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+    # AQUI ESTÁ A MÁGICA: Liberamos o :nome E o :role na hora de criar a conta (sign_up)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :telefone, :bio, :endereco, :avatar])
   end
 
   def authorize_admin!
