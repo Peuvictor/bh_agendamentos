ffca0d69d5f48c992dd579d6c8bb2c6c4d6a859e
Pedro Guimaraes <peuvictor22@gmail.com>
2026-05-06 14:24:16 -0300

    db: envia migracao da coluna duration para producao

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_24_175151) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_24_190735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_24_175151) do
     t.uuid "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "duration", default: 30
+    t.integer "duration"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
